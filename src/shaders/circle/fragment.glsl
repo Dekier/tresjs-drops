00d8e53826107a3e8dc0ac319e6001cab3b3f56a
Marcin Dekier <marcin.dekier@appunite.com>
2024-06-01 17:51:24 +0200

    remove assets

--- src/shaders/circle/fragment.glsl
+++
@@ -1,8 +0,0 @@
- uniform vec2 mousePos;
-void main() {
-    float dist = distance(gl_FragCoord.xy, mousePos);
-    if (dist < 0.1) {
-    // Logika ucieczki
-    }
-    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
-}
